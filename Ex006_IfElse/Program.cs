e735115de07b4e2337b73e04042f6425e8a4a85e
Pavel <Shestakov1996@yandex.ru>
2022-10-12 20:10:55 +0300

    операторы if else

---
+++ Ex006_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя:  ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "алина")
+{
+    Console.WriteLine("УРА,Это же АЛИНА!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
