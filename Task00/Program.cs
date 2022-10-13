77d842be56624d8b697cfaae0ba904df63999a38
Pavel <Shestakov1996@yandex.ru>
2022-10-13 21:21:38 +0300

    1 задача на семинаре

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход
+// 1.на вход принимает число 
+// 2.выдаёт его квадрат.
+
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+Console.Write ("Введите целое число:  ");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number ;
+Console.WriteLine($"Квадрат числа {number} = {square}");
