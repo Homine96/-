e8f85ded4b0a869cebb9dee8c228a672a0331b1e
Pavel <Shestakov1996@yandex.ru>
2022-10-15 16:42:14 +0300

    Заполнение и печать массива , нахождение индекса элемента массива с использованием методов

---
+++ Ex011_Array2/Program.cs
@@ -0,0 +1,66 @@
+﻿/*int [] array = {1,2,3,5,5,6,7,8,};
+int n = array.Length;
+int find = 5;
+int index = 0 ;
+
+ while (index<n)
+ {
+    if (array[index]==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+ }
+ */
+
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
