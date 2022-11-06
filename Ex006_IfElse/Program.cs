0a4ce7525ca6c5a9b349732d087000ee95420065
Pavel <Shestakov1996@yandex.ru>
2022-11-06 08:26:00 +0300

    update Ex006

--- Ex006_IfElse/Program.cs
+++ Ex006_IfElse/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.Write("Введите имя пользователя:  ");
 string? username = Console.ReadLine();
 
-if(username.ToLower() == "алина")
+if(username?.ToLower() == "алина")
 {
     Console.WriteLine("УРА,Это же моя любимая сладулечка барабулечка!");
 }
