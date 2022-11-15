0e4e17ac5be29da8645a04a71a269645b90229f9
Pavel <Shestakov1996@yandex.ru>
2022-11-15 21:22:59 +0300

    Задачи с семинара №6 ( одномерные массивы, копирование,разворот массива)

---
+++ Task40/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три
+// числа и проверяет, может ли существовать треугольник с сторонами
+// такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника
+// меньше суммы двух других сторон.
+
+Console.WriteLine("Введите сторону А");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите сторону B");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите сторону C");
+int c = Convert.ToInt32(Console.ReadLine());
+
+bool IsTriangle(int side1, int side2, int side3)
+{
+return side1 + side2 > side3 && side2 + side3 > side1 && side3 + side1 > side2;
+}
+bool result = IsTriangle(a,b,c);
+Console.Write( result == true? "Yes": "No" );
+// дальше вывод через тернарный оператор
---
+++ Task42/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+
+string СonverterDigit(int num)
+{
+    string res = "";
+    while (num > 0)
+    {
+        res = $"{(num % 2)}{res}";
+        num /= 2;
+    }
+    return res;
+}
+
+
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(СonverterDigit(a));
